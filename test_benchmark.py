19b7c083eb2cf92952cc0304aa40a4478fa759dd
Michael Coughlin <michael.w.coughlin@gmail.com>
2026-02-22 16:54:36 -0600

    Optimize linopy

--- test_benchmark.py
+++ test_benchmark.py
@@ -5,7 +5,10 @@ import cvxpy
 import docplex.mp.model
 import linopy
 import numpy as np
+import pandas as pd
 import pytest
+import xarray as xr
+from scipy.sparse import csc_matrix
 
 
 @pytest.fixture(scope="module", params=range(3))
@@ -23,11 +26,12 @@ def example_problem(request):
         np.vstack((rng.choice(nx, ncons), rng.choice(ny, ncons))), axis=1
     )
     assign = [edge_i[edge_j == j] for j in range(ny)]
+    A = csc_matrix((np.ones(len(edge_i)), (edge_j, edge_i)), shape=(ny, nx))
 
-    return nx, ny, k, w, assign
+    return nx, ny, k, w, assign, A
 
 
-def mwc0_docplex(nx, ny, k, w, assign):
+def mwc0_docplex(nx, ny, k, w, assign, A):
     with docplex.mp.model.Model() as m:
         x = np.asarray(m.binary_var_list(nx))
         y = np.asarray(m.binary_var_list(ny))
@@ -40,27 +44,24 @@ def mwc0_docplex(nx, ny, k, w, assign):
         return solution.get_values(x)
 
 
-def mwc1_cvxpy(nx, ny, k, w, assign):
+def mwc1_cvxpy(nx, ny, k, w, assign, A):
     x = cvxpy.Variable(nx, boolean=True)
     y = cvxpy.Variable(ny, boolean=True)
     problem = cvxpy.Problem(
         cvxpy.Maximize(y @ w),
-        [
-            x.sum() <= k,
-            *(cvxpy.sum(x[i]) >= y[j] for j, i in enumerate(assign)),
-        ],
+        [x.sum() <= k, A @ x >= y],
     )
     problem.solve(solver="cplex")
     return x.value
 
 
-def mwc2_linopy(nx, ny, k, w, assign):
+def mwc2_linopy(nx, ny, k, w, assign, A):
     m = linopy.Model()
-    x = m.add_variables(coords=[np.arange(nx)], binary=True)
-    y = m.add_variables(coords=[np.arange(ny)], binary=True)
+    x = m.add_variables(coords=[pd.RangeIndex(nx, name="i")], binary=True, name="x")
+    y = m.add_variables(coords=[pd.RangeIndex(ny, name="j")], binary=True, name="y")
     m.add_constraints(x.sum() <= k)
-    for j, i in enumerate(assign):
-        m.add_constraints(x.loc[i].sum() >= y[j])
+    A_xr = xr.DataArray(A.toarray(), dims=["j", "i"])
+    m.add_constraints(((A_xr * x).sum("i") >= y))
     m.add_objective(y @ w, sense="max")
     m.solve(solver_name="cplex")
     return x.solution
